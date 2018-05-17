f02fb3923c5066315e2a7fdbbe0363c3938e8d6d
rodrigo-ssj <rodrigodevs@gmail.com>
2018-05-17 18:11:46 +0100

    Added webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css'
+
+import { green } from 'chalk'
+
+console.log(green('This shall be green!'))
--- tools/devServer.js
+++ tools/devServer.js
@@ -1,10 +1,19 @@
-import path from 'path';
-import open from 'open';
-import chalk from 'chalk';
-import express from 'express';
+import path from 'path'
+import open from 'open'
+import chalk from 'chalk'
+import express from 'express'
+import webpack from 'webpack'
+import config from '../webpack.config'
+import webpackMiddleware from 'webpack-dev-middleware'
 
 const port = 3000
 const app = express()
+const compiler = webpack(config)
+
+app.use(webpackMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
 
 app.get('/', (req, res) => {
   res.sendFile(
